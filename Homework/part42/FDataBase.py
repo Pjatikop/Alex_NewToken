4f3adb41ab2b4b46690504e6fbd43e7bafd86af8
Pjatikop <Pjatikop@yandex.ru>
2023-05-27 14:31:59 +0300

    part 43

---
+++ Homework/part42/FDataBase.py
@@ -0,0 +1,61 @@
+import sqlite3
+import time
+import math
+import re
+from flask import url_for
+
+
+class FDataBase:
+    def __init__(self, db):
+        self.__db = db
+        self.__cur = db.cursor()
+
+    def get_menu(self):
+        sql = 'SELECT * FROM mainmenu'
+        try:
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res:
+                return res
+        except IOError:
+            print('Ошибка чтения из БД')
+        return []
+
+    def add_book(self, title, text, url):
+        try:
+            self.__cur.execute("SELECT COUNT() as 'count' FROM books WHERE url LIKE ?", (url,))
+            res = self.__cur.fetchone()
+            if res['count'] > 0:
+                print("Статья с таким URL уже существует")
+                return False
+
+            
+
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO books VALUES(NULL, ?, ?, ?, ?)", (title, text, url, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print('Ошибка добавления книги в БД ' + str(e))
+            return False
+        return True
+
+    def get_post(self, alias):
+        try:
+            self.__cur.execute(f"SELECT title, text FROM books WHERE url = '{alias}'")
+            res = self.__cur.fetchone()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print('Ошибка получения книги из БД ' + str(e))
+        return False, False
+
+    def get_books_annonce(self):
+        try:
+            self.__cur.execute("SELECT id, title, text, url FROM books ORDER BY time DESC")
+            res = self.__cur.fetchall()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print("Ошибка получения статей из БД " + str(e))
+        return []
+
--- Homework/part42/part_42.py
+++ Homework/part42/part_42.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template, url_for, request, flash, session, redirect
+from flask import Flask, render_template, url_for, request, flash, session, redirect, g
+import sqlite3
+import os
+from FDataBase import FDataBase
+
+DATABASE = '/tmp/flsk.db'
+DEBUG = True
+SECRET_KEY = '8f222bba2bb35888f80ec03a3e255cbc2a3d5ead'
+
+
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'jdghgblxi3874lvicn545nlncldklfjs'
+app.config.from_object(__name__)
+#app.config['SECRET_KEY'] = 'jdghgblxi3874lvicn545nlncldklfjs'
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsk.db')))
 
 menu = [
     {'name': 'Главная страница', 'url': 'books'},
@@ -10,11 +22,54 @@ menu = [
     {'name': 'Обратная связь', 'url': 'contact'}
 ]
 
+def connect_db():
+    con = sqlite3.connect(app.config['DATABASE'])
+    con.row_factory = sqlite3.Row
+    return con
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
 @app.route('/')
-@app.route('/books')
 def books():
-    print(url_for('books'))
-    return render_template('books.html', title='Главная страница магазина', menu=menu)
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('books.html', title='Главная страница библиотеки', menu=dbase.get_menu(),
+                           books=dbase.get_books_annonce())
+
+@app.route("/add_book", methods=["POST", "GET"])
+def add_book():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == 'POST':
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.add_book(request.form['name'], request.form['post'], request.form['url'])
+            if not res:
+                flash('Ошибка добавления книги', category='error')
+            else:
+                flash('Книга добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления книги', category='error')
+
+    return render_template('add_book.html', menu=dbase.get_menu(), title='Добавление книги')
+
+@app.route("/book/<alias>")
+def show_book(alias):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.get_post(alias)
+    return render_template('book.html', menu=dbase.get_menu(), title=title, post=post)
 
 @app.route('/items')
 def items():
@@ -59,6 +114,13 @@ def login():
         session['userLogged'] = request.form['username']
         return redirect(url_for('profile', username=session['userLogged']))
     return render_template('login.html', title='Авторизация', menu=menu)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
